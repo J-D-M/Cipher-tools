4b4ab616deaf66be525d30b61ad47a9e58490e92
Eyoson <Eyoson@waifu.club>
2018-04-02 10:35:50 -0700

    encode working

--- base64.c
+++ base64.c
@@ -1,4 +1,3 @@
-#include <assert.h>
 #include <stdbool.h>
 #include <stdint.h>
 #include <stdio.h>
@@ -8,8 +7,8 @@
 void usage(char *name)
 {
 	printf("Usage:\t%s [-d] [message...]\n\n"
-	       "\t-d: decrypt\n",
-	       name);
+			"\t-d: decrypt\n",
+			name);
 }
 
 char *flatten(char **argv, size_t, size_t);
@@ -32,9 +31,9 @@ int main(int argc, char **argv)
 	}
 
 	char *input =
-	    flatten(argv, (1 + decode_flag), argc); // skip first arg if
+		flatten(argv, (1 + decode_flag), argc); // skip first arg if its a flag
 
-	char *output = !strcmp("-d", argv[0]) ? decode(input) : encode(input);
+	char *output = (decode_flag) ? decode(input) : encode(input);
 
 	puts(output);
 
@@ -49,25 +48,27 @@ char *flatten(char **argv, size_t start, size_t end)
 {
 	size_t buffer_size = 0;
 
-	for (size_t i = 1; i < end; i++)
+	for (size_t i = start; i < end; i++)
 		buffer_size += strlen(argv[i]);
 
 	// spaces between strings +1 for null terminator
-	size_t spaces = end - 1;
-	char *str     = malloc(sizeof(char) * buffer_size + spaces);
+	size_t spaces = (end - start) - 1;
+	char *str     = malloc((sizeof(char) * buffer_size) + spaces);
+
 
 	size_t str_i = 0;
 
-	for (size_t i = 1; i < end; i++) {
-		for (size_t j = 0; argv[i][j]; j++) {
-			str[str_i] = argv[i][j];
+	while (start < end) {
+		for (char *j = argv[start]; *j; j++) {
+			str[str_i] = *j;
 			++str_i;
 		}
 
-		if (i < end - 1) {
+		if (start < end - 1) {
 			str[str_i] = ' ';
 			++str_i;
 		}
+		++start;
 	}
 
 	str[str_i] = '\0';
@@ -77,56 +78,68 @@ char *flatten(char **argv, size_t start, size_t end)
 
 // table for char values
 const char b64_char[] = {
-    'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-    'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
-    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/',
+	'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+	'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+	'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+	'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+	'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+', '/',
 };
-
-size_t getSize(char *str)
-{
-	size_t ret = strlen(str);
-
-	while ((ret * 8) % 24 != 0)
-		++ret;
-
-	return ret;
-}
 
 char *decode(char *input)
 {
 	char *ret_str = malloc(sizeof(char) * (strlen(input) + 1));
+	return ret_str;
+}
+
+// pad end if necessary
+size_t getSize(char *str)
+{
+	size_t size = strlen(str);
+	size_t ret = (size / 3) * 4 ;
+	ret += size % 3;
+	while (ret % 4 != 0)
+		++ret;
+
+	return ret + 1;
 }
 
 char *encode(char *input)
 {
-	assert(input != NULL);
 
-	char *ret_str    = malloc(sizeof(char) * (getSize(input) + 1));
+	size_t ret_str_size = getSize(input);
+
+
+	char *ret_str    = malloc(sizeof(char) * ret_str_size);
+
+
 	size_t itter     = 0;
 	size_t ret_itter = 0;
 	size_t max = strlen(input);
 
-	while (itter < max) {
-		uint64_t buffer = (input[itter] << 15);
+	while (itter <= max) {
+		uint64_t buffer = (input[itter] << 16);
 		if (itter + 1 < max)
-			(input[itter + 1] << 7);
+			buffer |= (input[itter + 1] << 8);
 		if (itter + 2 < max)
-			(input[itter + 2]);
+			buffer |= (input[itter + 2]);
 
-		for (int i = 0; i < 5; i++) {
+		// get appropriate bits for b64 char
+		for (int i = 0; i < 4 && i + itter <= max; i++) {
+
 			uint64_t tmp = buffer;
 			tmp >>= 6 * (3 - i);
-			tmp &= 0x3f;
+			tmp &= 0x3f; // six right most bits
 
 			ret_str[ret_itter] = b64_char[tmp];
 			++ret_itter;
 		}
 
-		input += 3;
+		itter += 3;
 	}
 
-	ret_str[ret_itter] = '\0';
+	while (ret_itter < ret_str_size - 1)
+		ret_str[ret_itter++] = '=';
+	ret_str[ret_itter++] = '\0';
+
 	return ret_str;
 }
